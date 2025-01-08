b9d080a5b5aa087c02767550ccb3c4317de153e5
alainware <irving6258@gmail.com>
2025-01-08 13:58:20 -0600

    update: added order content and order totals sections

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,23 @@
+import {OrderItem} from "../types";
+import {formatCurrency} from "../helpers";
+import {useMemo} from "react";
+
+type OrderTotalsProps = {
+    order: OrderItem[];
+    tip: number;
+}
+export default function OrderTotals({order, tip}: OrderTotalsProps) {
+    const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order]);
+    const tipAmount = useMemo(() => subtotalAmount * tip, [subtotalAmount, tip]);
+    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [subtotalAmount, tipAmount]);
+    return (
+        <>
+            <div className="space-y-3">
+                <h2 className="font-black text-2xl">Totales y Propinas:</h2>
+                <p>Subtotal a pagar: {""}<span className="font-bold">{formatCurrency(subtotalAmount)}</span></p>
+                <p>Propina: {""}<span className="font-bold">{formatCurrency(tipAmount)}</span></p>
+                <p>Total a pagar: {""}<span className="font-bold">{formatCurrency(totalAmount)}</span></p>
+            </div>
+        </>
+    )
+}
